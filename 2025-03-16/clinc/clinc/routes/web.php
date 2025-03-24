9cb7d534d7072c8d76c9d3cc0d796cec08e21200
mahmoudmagdy1-1 <d4rkppl1@gmail.com>
2025-03-24 23:52:08 +0200

    task submission

---
+++ 2025-03-16/clinc/clinc/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MajorController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('/majors', [MajorController::class, 'index'])->name('majors');
+Route::get('/doctors', [DoctorController::class, 'index'])->name('doctors');
+Route::get('/doctor', [DoctorController::class, 'show'])->name('doctor');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+
